e9402f4adcf814bd8d26abf0f4b782c03b9f9fe0
David J Birnbaum <djbpitt@gmail.com>
2016-05-08 10:29:12 -0400

    Initial commit of getYears.php, which displays list of years

---
+++ getYears.php
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>View the documents from a particular year</title>
+</head>
+<body>
+<h1>View the documents from a particular year</h1>
+<hr/>
+<?php
+    $contents = REST_PATH . "/db/queries/letterDates.xql";
+    $result = file_get_contents($contents);
+    echo $result;
+?>
+</table>
+</body>
+</html>
