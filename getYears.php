73e0fbcf1ec0a0d665b0d749686bb6a7fd64e386
David J Birnbaum <djbpitt@gmail.com>
2016-05-08 10:54:48 -0400

    More informative <title> and <h1> in getYears.php

--- getYears.php
+++ getYears.php
@@ -2,11 +2,12 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>View the documents from a particular year</title>
+    <title>View the years</title>
 </head>
 <body>
-<h1>View the documents from a particular year</h1>
+<h1>Years</h1>
 <hr/>
+<p>Select a year to view of list of letters from that year</p>
 <ul>
 <?php
     require_once("config.php");
